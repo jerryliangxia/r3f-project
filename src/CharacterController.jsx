137434e78109b1e271d6353862ef3db3bc7d90cf
Jerry Xia <jerryxia@Jerrys-MacBook-Pro.local>
2023-12-26 02:25:16 -0400

    WASD Controls, Jump relative to camera

--- src/CharacterController.jsx
+++ src/CharacterController.jsx
@@ -1,7 +1,7 @@
 import { useKeyboardControls, useAnimations, useGLTF } from "@react-three/drei";
 import { useEffect, useRef, useState } from "react";
 import { useFrame } from "@react-three/fiber";
-import { RigidBody, CapsuleCollider } from "@react-three/rapier";
+import { useRapier, RigidBody, CapsuleCollider } from "@react-three/rapier";
 import * as THREE from "three";
 
 function wrapAngle(angle) {
@@ -12,116 +12,154 @@ function wrapAngle(angle) {
 
 export default function CharacterController() {
   const body = useRef();
+  const { rapier, world } = useRapier();
+
   const character = useGLTF("./animated_spiderman_ps5.glb");
   const animations = useAnimations(character.animations, character.scene);
   const [characterState, setCharacterState] = useState("Idle");
   const [subscribeKeys, getKeys] = useKeyboardControls();
+  const [isJumping, setIsJumping] = useState(false);
 
   useFrame((state, delta) => {
-    const { forward, backward, leftward, rightward } = getKeys();
-    const keysPressed = [forward, backward, leftward, rightward].filter(
-      Boolean
-    ).length;
+    if (!isJumping) {
+      const { forward, backward, leftward, rightward } = getKeys();
+      const keysPressed = [forward, backward, leftward, rightward].filter(
+        Boolean
+      ).length;
 
-    // Only apply the impulse if two or fewer keys are pressed
-    if (keysPressed <= 2) {
-      // Get the camera's forward direction
-      const cameraDirection = new THREE.Vector3();
-      state.camera.getWorldDirection(cameraDirection);
+      // Only apply the impulse if two or fewer keys are pressed
+      if (keysPressed <= 2) {
+        // Get the camera's forward direction
+        const cameraDirection = new THREE.Vector3();
+        state.camera.getWorldDirection(cameraDirection);
 
-      // Project the camera direction onto the XZ plane
-      cameraDirection.y = 0;
-      cameraDirection.normalize();
+        // Project the camera direction onto the XZ plane
+        cameraDirection.y = 0;
+        cameraDirection.normalize();
 
-      // Create a rotation matrix from the camera's direction
-      const rotationMatrix = new THREE.Matrix4();
-      rotationMatrix.lookAt(
-        cameraDirection,
-        new THREE.Vector3(0, 0, 0),
-        state.camera.up
-      );
+        // Create a rotation matrix from the camera's direction
+        const rotationMatrix = new THREE.Matrix4();
+        rotationMatrix.lookAt(
+          cameraDirection,
+          new THREE.Vector3(0, 0, 0),
+          state.camera.up
+        );
 
-      // Initialize the input direction
-      const inputDirection = new THREE.Vector3(0, 0, 0);
+        // Initialize the input direction
+        const inputDirection = new THREE.Vector3(0, 0, 0);
 
-      if (forward) {
-        inputDirection.z += 1;
-      }
-      if (rightward) {
-        inputDirection.x -= 1;
-      }
-      if (backward) {
-        inputDirection.z -= 1;
-      }
-      if (leftward) {
-        inputDirection.x += 1;
-      }
+        if (forward) {
+          inputDirection.z += 1;
+        }
+        if (rightward) {
+          inputDirection.x -= 1;
+        }
+        if (backward) {
+          inputDirection.z -= 1;
+        }
+        if (leftward) {
+          inputDirection.x += 1;
+        }
 
-      // Transform the input direction from the camera's perspective to the world perspective
-      inputDirection.applyMatrix4(rotationMatrix);
-      // Get the current linear velocity of the character
-      const linvel = body.current.linvel();
+        // Transform the input direction from the camera's perspective to the world perspective
+        inputDirection.applyMatrix4(rotationMatrix);
+        // Get the current linear velocity of the character
+        const linvel = body.current.linvel();
 
-      // Calculate the magnitude of the linear velocity
-      const linvelMagnitude = Math.sqrt(linvel.x ** 2 + linvel.z ** 2);
-      // Only apply the impulse if the linear velocity is below a certain threshold
-      const maxLinvelMagnitude = 2; // Adjust this value to control the maximum speed
-      if (linvelMagnitude < maxLinvelMagnitude) {
-        // Use the transformed input direction for the impulse
-        const impulseStrength = 1 * delta;
-        const impulse = {
-          x: inputDirection.x * impulseStrength,
-          y: 0,
-          z: inputDirection.z * impulseStrength,
-        };
+        // Calculate the magnitude of the linear velocity
+        const linvelMagnitude = Math.sqrt(linvel.x ** 2 + linvel.z ** 2);
+        // Only apply the impulse if the linear velocity is below a certain threshold
+        const maxLinvelMagnitude = 2; // Adjust this value to control the maximum speed
+        if (linvelMagnitude < maxLinvelMagnitude) {
+          // Use the transformed input direction for the impulse
+          const impulseStrength = 1 * delta;
+          const impulse = {
+            x: inputDirection.x * impulseStrength,
+            y: 0,
+            z: inputDirection.z * impulseStrength,
+          };
 
-        if (impulse.x !== 0 || impulse.z !== 0) {
-          const targetAngle = Math.atan2(impulse.x, impulse.z);
-          const currentAngle = character.scene.rotation.y;
-          const newAngle = THREE.MathUtils.lerp(
-            currentAngle,
-            targetAngle,
-            0.05
-          );
+          if (impulse.x !== 0 || impulse.z !== 0) {
+            const targetAngle = Math.atan2(impulse.x, impulse.z);
+            const currentAngle = character.scene.rotation.y;
+            const newAngle = THREE.MathUtils.lerp(
+              currentAngle,
+              targetAngle,
+              0.1
+            );
 
-          // Wrap the new angle to ensure it's within the range of -π to π
-          const wrappedNewAngle = wrapAngle(newAngle);
+            // // Check if the angle change is large
+            const angleDifference = Math.abs(newAngle - currentAngle);
+            const angleThreshold = Math.PI; // Adjust this value to control the threshold
 
-          character.scene.rotation.y = wrappedNewAngle;
-          // Check if the character's rotation is close enough to the target angle
-          const angleTolerance = 0.1; // Adjust this value to control the rotation tolerance
+            console.log("Diff: " + angleDifference);
+            console.log("Threshold: " + angleThreshold);
 
-          // Check if the angle change is large
-          const angleDifference = Math.abs(newAngle - currentAngle);
+            // Only set the character's rotation if the angle change is not too large
+            if (angleDifference <= angleThreshold) {
+              character.scene.rotation.y = newAngle;
+              //   body.current.applyImpulse(impulse);
 
-          // Only apply the impulse if the character's rotation is close enough to the target angle
-          if (angleDifference < angleTolerance) {
-            body.current.applyImpulse(impulse);
+              // Check if the character's rotation is close enough to the target angle
+            }
+            const angleTolerance = 0.1; // Adjust this value to control the rotation tolerance
+
+            // Only apply the impulse if the character's rotation is close enough to the target angle
+            if (angleDifference < angleTolerance) {
+              body.current.applyImpulse(impulse);
+            }
+          }
+
+          // Determine the animation state based on the movement
+          const isMoving = impulse.x !== 0 || impulse.z !== 0;
+          if (isMoving && characterState !== "Run") {
+            setCharacterState("Run");
+          } else if (!isMoving && characterState !== "Idle") {
+            setCharacterState("Idle");
           }
         }
-
-        // Determine the animation state based on the movement
-        const isMoving = impulse.x !== 0 || impulse.z !== 0;
-        if (isMoving && characterState !== "Run") {
-          setCharacterState("Run");
-        } else if (!isMoving && characterState !== "Idle") {
-          setCharacterState("Idle");
-        }
+      } else {
+        setCharacterState("Idle");
       }
-
-      body.current.wakeUp();
-    } else {
-      setCharacterState("Idle");
     }
+    body.current.wakeUp();
   });
 
   useEffect(() => {
-    const action = animations.actions[characterState];
-    action.reset().fadeIn(0.5).play();
-    return () => {
-      action.fadeOut(0.5);
-    };
+    if (isJumping) {
+      const action = animations.actions[characterState];
+      action.reset().fadeIn(0.5).play();
+      setTimeout(() => {
+        setIsJumping(false);
+      }, 1500);
+      return () => {
+        action.fadeOut(0.5);
+      };
+    } else {
+      const action = animations.actions[characterState];
+      action.reset().fadeIn(0.5).play();
+      return () => {
+        action.fadeOut(0.5);
+      };
+    }
   }, [characterState]);
+
+  const jump = () => {
+    setIsJumping(true);
+    setCharacterState("Jump");
+  };
+
+  useEffect(() => {
+    const unsubscribeJump = subscribeKeys(
+      (state) => state.jump,
+      (value) => {
+        if (value) jump();
+      }
+    );
+    return () => {
+      unsubscribeJump();
+    };
+  });
 
   return (
     <>
@@ -131,7 +169,7 @@ export default function CharacterController() {
         position={[0, 1, -0.25]}
         scale={[0.5, 0.5, 0.5]}
         linearDamping={1}
-        // angularDamping={0.5}
+        angularDamping={0.5}
         enabledRotations={[false, false, false]}
       >
         <primitive
--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,7 +1,7 @@
 import { ContactShadows, Sky, OrbitControls } from "@react-three/drei";
 import { Perf } from "r3f-perf";
 import CharacterController from "./CharacterController";
-import { Physics, RigidBody, CapsuleCollider } from "@react-three/rapier";
+import { Physics, RigidBody } from "@react-three/rapier";
 
 export default function Experience() {
   return (
@@ -9,12 +9,12 @@ export default function Experience() {
       <Sky />
       <Perf position="top-left" />
 
-      <OrbitControls makeDefault />
+      <OrbitControls />
 
       <directionalLight castShadow position={[1, 2, 3]} intensity={2} />
       <ambientLight intensity={1} />
-      <ContactShadows position={[0, 0, 0]} />
-      <Physics debug>
+      <ContactShadows position={[0, 0.01, 0]} />
+      <Physics>
         <CharacterController />
         <RigidBody type="fixed" friction={1}>
           <mesh receiveShadow position-y={-1.25}>
